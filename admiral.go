9f957ca63bd539b7ef0ad54982ed095166d84d89
Will Stern <willrstern@gmail.com>
2015-11-10 11:56:30 -0600

    added better messaging

--- admiral.go
+++ admiral.go
@@ -11,10 +11,14 @@ var appName string
 
 func main() {
   var cmdDeploy = &cobra.Command{
-    Use:   "deploy [app to deploy]",
+    Use:   "deploy [app to deploy] [# of instances]",
     Short: "Deploy multiple instances of a fleet service",
     Long: `Deploy multiple instances of a Fleet service`,
     Run: func(cmd *cobra.Command, args []string) {
+      if len(args) < 2 {
+        println("Incorrect amount of arguments:\nadmiral deploy [app to deploy] [# of instances]")
+        return
+      }
       appName = args[0]
       scale, _ := strconv.ParseInt(args[1], 10, 64)
       deployApp(appName, int(scale), discovery, destroy)
@@ -27,6 +31,10 @@ func main() {
     Long: `Change running instances of a Fleet service
         `,
     Run: func(cmd *cobra.Command, args []string) {
+      if len(args) < 2 {
+        println("Incorrect amount of arguments:\nadmiral scale [app to deploy] [# of instances]")
+        return
+      }
       appName = args[0]
       scale, _ := strconv.ParseInt(args[1], 10, 64)
       scaleApp(appName, int(scale), discovery, destroy)
